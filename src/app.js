27e1e01d1fc0d40187c76a7fd5ca8169931fec97
santoshrazz <santoshrajbgp11@gmail.com>
2024-06-13 09:01:57 +0530

    finally resolved cors issue

--- src/app.js
+++ src/app.js
@@ -5,17 +5,18 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./Public"));
-app.use(
-  cors({
-    origin:
-      "https://darling-sable-969379.netlify.app/",
-    credentials: true,
-  })
-);
 
-app.options('*', cors({
-  origin: "https://darling-sable-969379.netlify.app/",
-  credentials: true,
+const allowedOrigins = ['https://aiccomputerinstitute.netlify.app'];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
 }));
 app.use(cookieParser());
 
